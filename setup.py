268faf20a5f43b6676d5a82b4112c41388baa42c
Pedro Mateus <pedro.mateus@maastro.nl>
2021-06-29 10:54:11 +0200

    Complete setup

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='roc_aggregator',
     version='1.0.0',
+    author='Pedro Mateus',
+    url='https://gitlab.com/UM-CDS/general-tools/roc-aggregator',
     description='ROC aggregator',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(include=['roc_aggregator', 'roc_aggregator.*']),
     install_requires=[
         'numpy >= 1.17'
