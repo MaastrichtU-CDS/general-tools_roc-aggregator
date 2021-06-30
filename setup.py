0591e4330cb8a9003dc7ddc8071877662957b5ed
Pedro Mateus <pedro.mateus@maastro.nl>
2021-06-30 11:16:14 +0200

    Improve setup

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+from os import path
 from setuptools import setup, find_packages
+
+current_directory = path.abspath(path.dirname(__file__))
+with open(path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='roc_aggregator',
-    version='1.0.0',
+    version='1.1.0',
     author='Pedro Mateus',
     url='https://gitlab.com/UM-CDS/general-tools/roc-aggregator',
     description='ROC aggregator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
